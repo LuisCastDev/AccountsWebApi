17b3d58132523a124acd05fb6a79f05822421560
luiisCastillo <90583642+LuisCastDev@users.noreply.github.com>
2023-04-07 21:02:58 -0400

    Setting the string connection

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using DB;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,13 +10,25 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AccountdbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AccountConnection"));
+});
+
 var app = builder.Build();
+using (var scope = app.Services.CreateScope()) 
+{
+
+    var context = scope.ServiceProvider.GetRequiredService<AccountdbContext>();
+        context.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
 }
 
 app.UseHttpsRedirection();
